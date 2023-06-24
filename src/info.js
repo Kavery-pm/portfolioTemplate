8a0da46bbd9119ababf9840f39b69e9bac502a5f
kavery-pm <shiny.kavery@gmail.com>
2023-06-24 16:59:49 +0200

    added the info data

---
+++ src/info.js
@@ -0,0 +1,84 @@
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+
+export const info = {
+    
+    firstName: "Kavery",
+    lastName: "Pandanda Madappa",
+    initials: "PM", 
+    position: "a FrontEnd Developer",
+  
+   
+    baseColor: colors[0],
+    miniBio: [ 
+        {
+            emoji: '☕',
+            text: 'fueled by coffee'
+        },
+        {
+            emoji: '🌎',
+            text: 'based in Germany'
+        },
+        
+        {
+            emoji: "📧",
+            text: "shiny.kavery@gmail.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://facebook.com",
+            icon: 'fa fa-facebook',
+            label: 'facebook'
+        },
+        {
+            link: "https://instagram.com",
+            icon: 'fa fa-instagram',
+            label: 'instagram'
+        },
+        {
+            link: "https://github.com",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://linkedin.com",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        },
+        {
+            link: "https://twitter.com",
+            icon: "fa fa-twitter",
+            label: 'twitter'
+        }
+
+
+    ],
+    bio: "Hello! I'm Kavery. I'm a Frontend Developer. I completed Master's in VTU,India. I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    skills:
+        {
+            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma','Angular','React native'],
+            exposedTo: ['nodejs', 'java']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'Video Games',
+            emoji: '🎮'
+        },
+        {
+            label: 'movies',
+            emoji: '🎥'
+        },
+        {
+            label: 'cooking',
+            emoji: '🌶'
+        }
+
+    ],
+    
+    
+}

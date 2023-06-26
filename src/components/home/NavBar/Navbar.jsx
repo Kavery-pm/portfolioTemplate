22fb6c73bbcfeb8a1add744e663d1e60d29b3c3b
kavery-pm <shiny.kavery@gmail.com>
2023-06-26 17:20:04 +0200

    added link

--- src/components/home/NavBar/Navbar.jsx
+++ src/components/home/NavBar/Navbar.jsx
@@ -1,9 +1,13 @@
 import {Box} from "@mui/material";
 import Style from './Navbar.module.scss';
-import {Link} from "react-router-dom";
+import {Link,useLocation} from "react-router-dom";
+import { useState } from "react";
 let colors = [" rgb(131,58,180),rgba(131, 58, 180, 1) 0%, rgba(253, 29, 29, 1) 50%, rgba(252, 176, 69, 1) 100%"];
 const Navbar = ()=>{
-
+    const location = useLocation();
+    console.log(location.pathname)
+   
+    const [active, setActive] = useState(location.pathname === '/' ? 'home' : location.pathname.slice(1, location.pathname.length));
     const links = [
         {
             name: 'Home',
@@ -22,7 +26,7 @@ return (
                  textTransform={'lowercase'} fontSize={'1rem'}>
                 {links.map((link, index) => (
                     <Box key={index} component={'li'} 
-                    sx={{borderImageSource:  `linear-gradient(135deg, ${colors})`}} className={Style.active}>
+                    sx={{borderImageSource:  `linear-gradient(135deg, ${colors})`}}  className={(link.active === active && !link.type) && Style.active}>
                              <Link to={link.to}  className={Style.link}>
                             {!link.type && <p style={{padding: '0.5rem 0'}}>{link.name}</p>}
                             {link.type && <h1>{link.name}</h1>}

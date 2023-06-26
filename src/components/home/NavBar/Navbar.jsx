2df5d07c56e232738e6f87cecbe4765f0b91aa3b
kavery-pm <shiny.kavery@gmail.com>
2023-06-26 17:10:47 +0200

    added navbar styling

--- src/components/home/NavBar/Navbar.jsx
+++ src/components/home/NavBar/Navbar.jsx
@@ -1,5 +1,9 @@
 import {Box} from "@mui/material";
+import Style from './Navbar.module.scss';
+import {Link} from "react-router-dom";
+let colors = [" rgb(131,58,180),rgba(131, 58, 180, 1) 0%, rgba(253, 29, 29, 1) 50%, rgba(252, 176, 69, 1) 100%"];
 const Navbar = ()=>{
+
     const links = [
         {
             name: 'Home',
@@ -18,8 +22,11 @@ return (
                  textTransform={'lowercase'} fontSize={'1rem'}>
                 {links.map((link, index) => (
                     <Box key={index} component={'li'} 
-                        >
-                            <a href='/'>Home</a>
+                    sx={{borderImageSource:  `linear-gradient(135deg, ${colors})`}} className={Style.active}>
+                             <Link to={link.to}  className={Style.link}>
+                            {!link.type && <p style={{padding: '0.5rem 0'}}>{link.name}</p>}
+                            {link.type && <h1>{link.name}</h1>}
+                        </Link>
                        
                     </Box>
                 ))}
